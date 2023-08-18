5339752c69634a81b4683130a9c4407cf64bae2c
max <u5190335@anu.edu.au>
2023-08-18 10:39:49 +1000

    overriding default values to work with medportal

--- deposit_with_openjournals.rb
+++ deposit_with_openjournals.rb
@@ -5,7 +5,24 @@ paper_path = ENV["PAPER_PATH"].to_s
 journal_alias = ENV['JOURNAL_ALIAS']
 journal_secret = ENV['JOURNAL_SECRET']
 
-journal = Theoj::Journal.new(Theoj::JOURNALS_DATA[journal_alias.to_sym])
+# I must override the journal otherwise it looks in the wrong repo for the issue.
+# journal_alias = ENV["JOURNAL_ALIAS"]
+# journal = Theoj::Journal.new(Theoj::JOURNALS_DATA[journal_alias.to_sym])
+
+journal_data = {
+  doi_prefix: "10.00000",
+  url: "https://medportal-dev-6a745f452687.herokuapp.com/",
+  name: "ACCESS-NRI MedPortal",
+  alias: "MedPortal",
+  launch_date: "2023-08-14",
+  papers_repository: "ACCESS-NRI/med-recipes",
+  reviews_repository: "ACCESS-NRI/med-reviews",
+  deposit_url: "https://medportal-dev-6a745f452687.herokuapp.com/papers/api_deposit",
+  retract_url: "https://medportal-dev-6a745f452687.herokuapp.com/papers/api_retract"
+}
+
+journal = Theoj::Journal.new(journal_data)
+
 issue = Theoj::ReviewIssue.new(journal.data[:reviews_repository], issue_id)
 issue.paper = Theoj::Paper.new("", "", paper_path) unless paper_path.empty?
 
