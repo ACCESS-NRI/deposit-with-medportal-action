170649fb03daae520bfa5730277f28385998b36a
max <u5190335@anu.edu.au>
2023-09-14 10:40:20 +1000

    padding DOI

--- deposit_with_openjournals.rb
+++ deposit_with_openjournals.rb
@@ -1,41 +1,10 @@
 require "faraday"
 require "securerandom"
 
-#paper_path = ENV["PAPER_PATH"].to_s
-
-# journal_data = {
-#   doi_prefix: "10.21105",
-#   url: "https://medportal-dev-6a745f452687.herokuapp.com/",
-#   name: "ACCESS-NRI MedPortal",
-#   alias: "medportal",
-#   launch_date: "2023-08-14",
-#   papers_repository: "ACCESS-NRI/med-recipes",
-#   reviews_repository: "ACCESS-NRI/med-reviews",
-#   deposit_url: "https://medportal-dev-6a745f452687.herokuapp.com/papers/api_deposit",
-#   retract_url: "https://medportal-dev-6a745f452687.herokuapp.com/papers/api_retract"
-# }
-
-# journal = Theoj::Journal.new(journal_data)
-
-# issue = Theoj::ReviewIssue.new(journal.data[:reviews_repository], issue_id)
-# issue.paper = Theoj::Paper.new("", "", paper_path) unless paper_path.empty?
-
-# submission = Theoj::Submission.new(journal, issue, issue.paper)
-
-# deposit_call = submission.deposit!(journal_secret)
-
-# if deposit_call.status.between?(200, 299)
-#   system("echo 'Journal responded. Deposit looks good'")
-#   system("echo 'paper_doi=#{submission.paper_doi}' >> $GITHUB_OUTPUT")
-# else
-#   system("echo 'CUSTOM_ERROR=Could not deposit with Open Journals.' >> $GITHUB_ENV")
-#   raise "!! ERROR: Something went wrong with this deposit when calling #{journal.data[:deposit_url]}"
-# end
-
 issue_id = ENV["ISSUE_ID"]
 journal_secret = ENV['JOURNAL_SECRET']
 deposit_url = "https://medportal-dev-6a745f452687.herokuapp.com/papers/api_deposit"
-doi = "10.21105/medportal.#{issue_id}"
+doi = "10.21105/medportal.#{issue_id.to_s.rjust(5, '0')}"
 title = SecureRandom.hex
 
 def metadata_payload(title, issue_id, doi)
